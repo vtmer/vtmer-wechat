c81218b5579ad42e53f4a95cfd904c8fc785c895
MaureenChan <maureencqm@gmail.com>
2015-02-01 08:58:03 +0000

    fix code style

--- app/database/migrations/2015_01_24_063048_contact.php
+++ app/database/migrations/2015_01_24_063048_contact.php
@@ -5,13 +5,13 @@ use Illuminate\Database\Migrations\Migration;
 
 class ContactMigrate extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
         Schema::create('contact', function($table) {
             $table->increments('id');
             $table->string('name', 10)->nullable(false);
@@ -19,16 +19,16 @@ class ContactMigrate extends Migration {
             $table->integer('short_number')->nullable(true);
             $table->integer('qq')->nullable(true);
         });
-	}
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
         Schema::dropIfExists('contact');
-	}
+    }
 
 }
